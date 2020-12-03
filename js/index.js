5b398ceaef410c9d6b81ec7ac89681dd5008e3bf
yosef <yosef3053@gmail.com>
2020-12-03 09:16:54 +0200

    m

--- js/index.js
+++ js/index.js
@@ -4,11 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
 var userRouter_1 = require("./userRouter");
 var areaRouter_1 = require("./areaRouter");
 var gateRouter_1 = require("./gateRouter");
 var port = process.env.PORT || 3000;
 var app = express_1.default();
+app.use(cors_1.default());
 app.use(express_1.default.json({ limit: '5mb' }));
 app.use('/user', userRouter_1.router);
 app.use('/area', areaRouter_1.router);
--- ts/index.ts
+++ ts/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 import { router as userRouter } from './userRouter'
 import { router as areaRouter } from './areaRouter'
 import { router as gateRouter } from './gateRouter'
 const port = process.env.PORT || 3000
 
 const app = express()
+app.use(cors())
 app.use(express.json({ limit: '5mb' }))
 app.use('/user', userRouter)
 app.use('/area', areaRouter)
